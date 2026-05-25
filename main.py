75017eb4aba67a1cf9ee6b2ea183a7cf5e57d11b
Dharmik <dharmik.madupathi@gmail.com>
2026-05-24 22:05:39 -0500

    added some things

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ socketio = SocketIO(app)
 #this is the base home root for the flask app and has the methods of POST and GET to allow for retreiving the home page and also sending the data when you interact with the chat 
 @app.route("/", methods = ["POST", "GET"])
 def home():
+    #when the browser talks to a website it uses different methods. The Post is for sending data to the server from the website 
+    if request.method == "POST" :
+        #These lines pull out the information that the user inputted for the name and the code. 
+        name = request.form.get("name")
+        code = request.form.get("code")
+        #This gets the information from the button pressed to see if it was check and if it was not check it would return false instead of None with the addition of ,False
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+        #if the name is not inputted then it will turn it into a web page to send to the user browser with a variable named error to the home.html file
+        if not name: 
+            return render_template("home.html", error= "Please enter a name ")
+
     #this is the connection from the backend to the front end to output the HTML/javascript front end of the website. 
     return render_template("home.html")
 
